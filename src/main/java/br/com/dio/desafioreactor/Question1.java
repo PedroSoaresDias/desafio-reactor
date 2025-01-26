9eb13a788779a4bcc6ca79473876956360db1b2b
Pedro Soares Dias <pedroferraridias@gmail.com>
2025-01-26 11:10:18 -0300

    Resolvendo o desafio

--- src/main/java/br/com/dio/desafioreactor/Question1.java
+++ src/main/java/br/com/dio/desafioreactor/Question1.java
@@ -5,10 +5,13 @@ import reactor.core.publisher.Flux;
 import java.util.List;
 
 public class Question1 {
-
     /*
-    Recebe uma lista de longs, incrementa 1 nos valores e retorna um flux dos resultados
+     * Recebe uma lista de longs, incrementa 1 nos valores e retorna um flux dos
+     * resultados
      */
-    public Flux<Long> inc(final List<Long> numbers){}
+    public Flux<Long> inc(final List<Long> numbers) {
+        return Flux.fromIterable(numbers)
+                .map(n -> n + 1L);
+    }
 
 }
--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,12 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user) {
+        if (user.password().length() <= 8) {
+            return Mono.error(new IllegalArgumentException("A senha precisa ser mais de 8 caracteres"));
+        }
+
+        return Mono.empty();
+    }
 
 }
